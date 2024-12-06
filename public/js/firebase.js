27685b4414af864015bdf7182a2aa3cb15ea982c
Anoshaa <160060320+Anoshaa@users.noreply.github.com>
2024-12-06 23:53:45 +0500

    blog

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBLoIuExl01cEIIUjhUX1tiyP5_emvdEcw",
+    authDomain: "blogging-website-55af7.firebaseapp.com",
+    projectId: "blogging-website-55af7",
+    storageBucket: "blogging-website-55af7.firebasestorage.app",
+    messagingSenderId: "286010962969",
+    appId: "1:286010962969:web:c2b7f87bd2ff4733116688"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  let db = firebase.firestore();
